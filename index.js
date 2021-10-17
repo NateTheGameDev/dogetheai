8d8eb389d2a394145609f319234cf87d1d2c1e52
NateTheGameDev <90973438+NateTheGameDev@users.noreply.github.com>
2021-10-17 17:14:34 -0400

    Update index.js

--- index.js
+++ index.js
@@ -193,5 +193,5 @@ function player2keysUp(e) {
 window.addEventListener("keydown",keyDown)
 window.addEventListener("keyup",keyUp)
 //draw the game loop
-//dont touch this code on the bottom or the game will stop working
+//dont mess this code on the bottom or the game will stop working
 Draw();
